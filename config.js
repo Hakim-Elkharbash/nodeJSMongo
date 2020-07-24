8871b4075615b6d6f4db7e8177f733301370cfd4
Hakim Elkharbash <hakem_g@yahoo.com>
2020-07-24 13:05:15 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '961692810919317',
+        clientSecret: '087d60edebd7d7f3fa1e172c1c65385b'
+    }
 }
